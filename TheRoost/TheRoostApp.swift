8daa95a6b00b2f6af65aec86bb4c24df61e04d21
SusomHait <susom.hait@gmail.com>
2026-03-30 11:16:29 -0500

    Initial Commit

---
+++ TheRoost/TheRoostApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheRoostApp.swift
+//  TheRoost
+//
+//  Created by Susom Hait on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct TheRoostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
